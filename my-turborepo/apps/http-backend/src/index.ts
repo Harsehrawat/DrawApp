d0542fbbe0d45df36a73d239e95eee7d9fb4ee6e
Harsh Sehrawat <harsehrawat@gmail.com>
2025-07-04 11:01:42 +0530

    set-up all imports from other packages

--- my-turborepo/apps/http-backend/src/index.ts
+++ my-turborepo/apps/http-backend/src/index.ts
@@ -1,5 +1,9 @@
 import express from "express";
 
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/config";
+import { prismaClient } from "@repo/db/config";
+
 const app = express();
 
 
