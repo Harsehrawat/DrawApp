b51ecdf9f45315baa001ae9ea6a9f024fc5d4fdb
Harsh Sehrawat <harsehrawat@gmail.com>
2025-07-05 00:14:42 +0530

    added signin/signup api

--- my-turborepo/apps/http-backend/src/index.ts
+++ my-turborepo/apps/http-backend/src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { CreateUserSchema,SignInSchema,  CreateRoomSchema } from "@repo/common/config";
 import { prismaClient } from "@repo/db/config";
+import { ZodIssue} from "zod";
 import jwt  from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
@@ -11,10 +12,11 @@ app.use(express.json());
 
 app.post("/api/signup", async (req, res) => {
     // First: validate using Zod
+
     const verifyZod = CreateUserSchema.safeParse(req.body);
 
     if (!verifyZod.success) {
-        const error = verifyZod.error.errors.map((err) => ({
+        const error = verifyZod.error.errors.map((err: ZodIssue) => ({
             field: err.path[0],
             message: err.message
         }));
@@ -69,51 +71,53 @@ app.post("/api/signup", async (req, res) => {
     }
 });
 
-// app.post("/api/signin", async (req,res)=>{
-//     // first verify format of i/p username and password
-//     const parsedInputs = SignInSchema.safeParse(req.body);
-//     if(!parsedInputs.success){
-//         // get the errors
-//         const errors = parsedInputs.error.errors.map( (err) => ({
-//             field: err.path[0],
-//             message: err.message
-//         }));
-//         res.status(400).json({
-//             success: false,
-//             message: errors
-//         })
-//         return;
-//     }
-//     // verify the username , then password from existing db entries
-//     try{
-//         const {username,password} = req.body;
-//         const user = await prismaClient.user.findFirst({
-//             where: {email: username}
-//         })
-//         if(!user){
-//             res.status(400).json({ success: false, message: "wrong username"});
-//             return;
-//         }
+app.post("/api/signin", async (req,res)=>{
+    // first verify format of i/p username and password
+    const parsedInputs = SignInSchema.safeParse(req.body);
+    if(!parsedInputs.success){
+        // get the errors
+        const errors = parsedInputs.error.errors.map( (err: ZodIssue) => ({
+            field: err.path[0],
+            message: err.message
+        }));
+        res.status(400).json({
+            success: false,
+            message: errors
+        })
+        return;
+    }
+    // verify the username , then password from existing db entries
+    try{
+        const {username,password} = req.body;
+        const user = await prismaClient.user.findFirst({
+            where: {email: username}
+        })
+        if(!user){
+            res.status(400).json({ success: false, message: "wrong username"});
+            return;
+        }
         
-//         // now verify password
-//         const verify_DB_Password = await bcrypt.compare( password,user.password);
-//         if(!verify_DB_Password){
-//             res.status(400).json({ success: false, message: "wrong password"});
-//             return;
-//         }
+        // now verify password
+        const verify_DB_Password = await bcrypt.compare( password,user.password);
+        if(!verify_DB_Password){
+            res.status(400).json({ success: false, message: "wrong password"});
+            return;
+        }
 
-//         // valid user, generate and return a jwt token
-//         const token = jwt.sign( {userId : user.id, userEmail: user.email}, JWT_SECRET, {expiresIn: "6h"});
-//         // return this to user
-//         res.status(200).json({ success: true, message: "user signin successfull", token});
-//         return;
-//     }catch(e){
-//         console.log("error at /api/signin, error : "+e);
-//         res.status(500).json({ success: false, message: "Server Error"});
-//     }
-// })
-
-
+        // valid user, generate and return a jwt token
+        const token = jwt.sign( {userId : user.id, userEmail: user.email}, JWT_SECRET, {expiresIn: "6h"});
+        // return this to user
+        res.status(200).json({ success: true, message: "user signin successfull", token});
+        return;
+    }catch(e){
+        console.log("error at /api/signin, error : "+e);
+        res.status(500).json({ success: false, message: "Server Error"});
+    }
+})
 
 
-app.listen(3001);
+
+
+app.listen(3001, ()=>{
+    console.log("server successfully started at port : 3001");
+});
