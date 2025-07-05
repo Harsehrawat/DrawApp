e7f6f15a3d894083b2c1014e40eb525c817956a9
Harsh Sehrawat <harsehrawat@gmail.com>
2025-07-05 05:51:17 +0530

    corrected import for SignInSchema from common

--- my-turborepo/apps/ws-backend/src/index.ts
+++ my-turborepo/apps/ws-backend/src/index.ts
@@ -1,6 +1,6 @@
 import { WebSocketServer } from "ws";
 import { JWT_SECRET } from "@repo/backend-common/config";
-import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/config";
+import { CreateUserSchema, SignInSchema, CreateRoomSchema } from "@repo/common/config";
 import { prismaClient } from "@repo/db/config";
 
 const wss = new WebSocketServer({port : 8081});
